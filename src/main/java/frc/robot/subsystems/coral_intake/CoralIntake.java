f37c62f70b531144a87821bffe1487cc2cad665b
restofnate <restofnate@gmail.com>
2025-02-04 07:42:50 -0500

    Worked on intake and then commented it out

--- src/main/java/frc/robot/subsystems/coral_intake/CoralIntake.java
+++ src/main/java/frc/robot/subsystems/coral_intake/CoralIntake.java
@@ -1,3 +1,52 @@
 package frc.robot.subsystems.coral_intake;
 
-public class CoralIntake {}
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+
+public class CoralIntake extends SubsystemBase {
+  // private SparkMax intakeMotor = new
+  // SparkMax(CoralIntakeConstants.intakeCANId.intakeMotor, MotorType.kBrushless);
+  // private SparkMax rotationMotor = new
+  // SparkMax(CoralIntakeConstants.intakeCANId.rotationMotor,
+  // MotorType.kBrushless);
+
+  // private double initialIntakePosiiion;
+
+  // public CoralIntake() {
+  // intakeMotor.configure(null, null, null);
+  // rotationMotor.configure(null, null, null);
+  // }
+
+  // public void setCoralIntakeVoltage(double voltage) {
+  // intakeMotor.set(voltage);
+  // }
+
+  // public void setRotationMotorVoltage(double voltage) {
+  // rotationMotor.set(voltage);
+  // }
+
+  // public void setIntakeMotorSpeed(double speed) {
+  // intakeMotor.set(speed);
+  // }
+
+  // public void setRotationMotorSpeed(double speed) {
+  // rotationMotor.set(speed);
+  // }
+
+}
