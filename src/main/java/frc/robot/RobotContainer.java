7350cb20550e77796cb5d230baf7560f0cdec493
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2025-02-15 23:27:07 -0500

    Sim running

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -28,6 +28,8 @@ import frc.robot.subsystems.drive.odometry_threads.PhoenixOdometryThread;
 import frc.robot.subsystems.drive.odometry_threads.SparkOdometryThread;
 import frc.robot.subsystems.position_joint.PositionJoint;
 import frc.robot.subsystems.position_joint.PositionJointConstants;
+import frc.robot.subsystems.position_joint.PositionJointIOReplay;
+import frc.robot.subsystems.position_joint.PositionJointIOSim;
 import frc.robot.subsystems.position_joint.PositionJointIOSparkMax;
 import frc.robot.subsystems.vision.Vision;
 import frc.robot.subsystems.vision.VisionConstants;
@@ -182,14 +184,18 @@ public class RobotContainer {
                         null,
                         null);
 
+                rightCoralRotationMotor = new PositionJoint(
+                        new PositionJointIOSim(
+                                "RightCoralRotateMotor",
+                                PositionJointConstants.RIGHT_CORAL_INTAKE_RROTATION_CONFIG),
+                        PositionJointConstants.CORAL_INTAKE_ROTATION_GAINS);
+
                 vision = new Vision(
                         drive::addVisionMeasurement,
                         new VisionIOPhotonVisionSim(
-                                VisionConstants.camera0Name,
-                                VisionConstants.robotToCamera0, drive::getPose),
+                                VisionConstants.camera0Name, VisionConstants.robotToCamera0, drive::getPose),
                         new VisionIOPhotonVisionSim(
-                                VisionConstants.camera1Name,
-                                VisionConstants.robotToCamera1, drive::getPose));
+                                VisionConstants.camera1Name, VisionConstants.robotToCamera1, drive::getPose));
                 break;
 
             default:
@@ -219,6 +225,11 @@ public class RobotContainer {
                                 AzimuthMotorConstants.Azimuth_GAINS),
                         null,
                         null);
+
+                rightCoralRotationMotor = new PositionJoint(
+                        new PositionJointIOReplay("RightCoralRotateMotor"),
+                        PositionJointConstants.CORAL_INTAKE_ROTATION_GAINS);
+
                 vision = new Vision(drive::addVisionMeasurement, new VisionIO() {
                 }, new VisionIO() {
                 });
